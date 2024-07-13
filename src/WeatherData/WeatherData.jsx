071fb805a188238fef6fa9aeae120b5c08c4db3e
harish <harishnagubadi9190@gmail.com>
2024-07-13 16:53:47 +0530

    final commit

--- src/WeatherData/WeatherData.jsx
+++ src/WeatherData/WeatherData.jsx
@@ -10,20 +10,29 @@ const Weather = () => {
   const [location, setLocation] = React.useState("")
   
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c73e6df6a5e5b9702ea5b3f50a6de072`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c73e6df6a5e5b9702ea5b3f50a6de072`
 
-  const searchLocation = (event) => {
+  const searchLocation = async(event) => {
     if (event.key === "Enter") {
       
-      axios.get(url)
-        .then((res) => {
-          setData(res.data)
+      try
+      {
+        const res =await axios.get(url)
+        setData(res.data)
+        setLocation("")
+        console.log(res.data)
+
+        if(res.status === 404)
+        {
+          alert("city not available")
           console.log(res.data)
-          setLocation("")
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error)
-        })
+        }
+      }
+
+      catch(err)
+      {
+        console.log(err)
+      }
     }
   }
   
@@ -40,28 +49,34 @@ const Weather = () => {
       <div className='content'>
 
         <div className='top'>
+        <div>
           <p className='location'>{data.name}</p>
-         {data.main ? <h1 className='temp'>{data.main.temp} °F</h1> : null}
+        </div>
+        <div>
+         {data.main ? <h2 className='temp'>{data.main.temp} °c</h2> : null}
+        </div>
+        <div>
           {data.weather ? <p className='description'>{data.weather[0].main}</p> : null}
+        </div>
         </div>
 
         <div className='bottom'>
 
           <div className='feels'>
             {data.main ? <p className='bold'>{data.main.feels_like} °F</p> : null}
-            <p>Feels Like</p>
+            {data.main ? <p>Feels Like</p> : null }
             
           </div>
 
           <div className='humidity'>
             {data.main ? <p className='bold'> {data.main.humidity}%</p> : null}
-            <p>Humidity</p>
+            {data.main ? <p>Humidity</p> : null}
             
           </div>
 
           <div className='wind'>
             {data.wind ? <p className='bold'>{data.wind.speed}MPH</p> : null}
-            <p>Wind Speed</p>
+            {data.wind ? <p>Wind Speed</p> : null}
             
           </div>
 
